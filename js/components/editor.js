1cfa8b0c9182cf7308d3ecbf01fdd58fbccd0236
Jes Gundy <jesse.gundy@gmail.com>
2014-07-11 23:56:03 -0400

    completed basic editor session setup

--- js/components/editor.js
+++ js/components/editor.js
@@ -13,7 +13,31 @@ define([
 
     // startup
     initialize: function() {
+      // build the editor session
+      this.buildSession();
+
+      // build the editor
+      this.buildEditor();
+
+      // focuson the editor
+      this.editor.focus();
+    },
+
+
+    // Build the Editor Session
+    buildSession: function() {
+      this.session = ace.createEditSession("# This is a test");
+      this.session.setMode("ace/mode/markdown");
+      this.session.setTabSize(2);
+      this.session.setUseSoftTabs(true);
+    },
+
+
+    // Build the Editor
+    buildEditor: function() {
       this.editor = ace.edit( this.el );
+      this.editor.setTheme("ace/theme/twilight");
+      this.editor.setSession( this.session );
     }
 
 
